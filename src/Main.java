11f83ef8ff144ce4bed24298e4628c41b9f0ae16
Danila Leonidov <3317451@gmail.com>
2024-02-29 22:58:51 +0300

    Change src

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int ticketCost = 13676; // стоимость билета в рублях
+        int miles20Rubles = 1; // количество миль за каждые 20 рублей
+
+        // Рассчет количества бонусных миль
+        int miles = ticketCost / 20 * miles20Rubles;
+
+        System.out.println("За билет стоимостью " + ticketCost + " рублей начислено " + miles + " миль.");
     }
 }
